734e2618d2308219c859d9a06e4ecbe5193fbb7e
Abdessattar <ghabi.1602@gmail.com>
2023-08-11 09:54:42 -0700

    Bug fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
-/*
- * main - Entry point
+/**
+ * main - prints a text
  *
- * Description: prints "Programming is like building a multilingual puzzle"
+ * Description: prints "\"Programming is like building a multilingual puzzle"
  *
  * Return: Always 0 (Success)
  *
@@ -11,6 +11,6 @@
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
