7bb8dfdf018ae697f5260f3836816eaeda035791
Abdessattar <ghabi.1602@gmail.com>
2023-08-12 17:50:17 -0700

    Print alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabets
+ * Description: prints the alphabets using putchar()
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int c = 97;
+
+	while (c <= 123)
+	{
+		putchar(c);
+		c++;
+	}
+	putchar(10);
+	return (0);
+}
