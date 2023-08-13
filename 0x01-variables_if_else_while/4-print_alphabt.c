2183ff3573127b9f7ed5f37b6c828ae9aab146a0
Abdessattar <ghabi.1602@gmail.com>
2023-08-12 18:12:09 -0700

    Print alphabets

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabets except q and e
+ * Description: prints the alphabets except q and e using putchar()
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int c = 97;
+
+	while (c < 123)
+	{
+		if (c == 101 || c == 113)
+			continue;
+		else
+		{
+		putchar(c);
+		c++;
+		}
+	}
+	putchar(10);
+	return (0);
+}
