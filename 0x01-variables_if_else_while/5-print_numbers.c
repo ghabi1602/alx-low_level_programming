6000188c4a8fb953dbc279129285572d1a6107a2
Abdessattar <ghabi.1602@gmail.com>
2023-08-13 07:25:25 -0700

    Print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints numbers
+ * Description: prints numbers from 0 to 9
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int c = 48;
+
+	while (c < 58)
+	{
+		putchar(c);
+		c++;
+	}
+	return (0);
+}
