e87eed938a0a72f6fb5c61d33d7b97067f082512
Abdessattar <ghabi.1602@gmail.com>
2023-08-14 16:43:15 -0700

    Print two two digits comb

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+/**
+ * main - prints numbers
+ * Description: prints two two digit numbers
+ * @a
+ * @b
+ * @c
+ * @d
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int a = 48;
+	int b = 48;
+	int c = 48;
+	int d = 48;
+
+	while (a < 58)
+	{
+		b = 48;
+		while (b < 57)
+		{
+			while (c < 58)
+			{
+
+				while (d < 58)
+				{
+					putchar(a);
+					putchar(b);
+					putchar(32);
+					putchar(c);
+					putchar(d);
+					if (a < 58 || b < 57 || c < 58 || d < 58)
+					{
+						putchar(44);
+						putchar(32);
+					}
+					d++;
+				}
+				c++;
+				d = 48;
+			}
+			b++;
+			c = a;
+			d = b + 1;
+		}
+		a++;
+	}
+	putchar(10);
+	return (0);
+}
