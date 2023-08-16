6769d719b157de15072d4d73e5df27ae67b21399
Abdessattar <ghabi.1602@gmail.com>
2023-08-15 20:43:20 -0700

    fix some code issues

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -11,7 +11,7 @@ void jack_bauer(void)
 {
 	int i, j, k, l;
 
-	for (i = 0; i < 3; i++)
+	for (i = 0; i < 2; i++)
 	{
 		for (j = 0; j < 10; j++)
 		{
@@ -19,13 +19,28 @@ void jack_bauer(void)
 			{
 				for (l = 0; l < 10; l++)
 				{
-					_putchar(i);
-					_putchar(j);
+					_putchar(i + '0');
+					_putchar(j + '0');
 					_putchar(':');
-					_putchar(k);
-					_putchar(l);
+					_putchar(k + '0');
+					_putchar(l + '0');
 					_putchar('\n');
 				}
+			}
+		}
+	}
+	for (j = 0; j < 4; j++)
+	{
+		for (k = 0; k < 6; k++)
+		{
+			for (l = 0; l < 10; l++)
+			{
+				_putchar('2');
+				_putchar(j + '0');
+				_putchar(':');
+				_putchar(k + '0');
+				_putchar(l + '0');
+				_putchar('\n');
 			}
 		}
 	}
