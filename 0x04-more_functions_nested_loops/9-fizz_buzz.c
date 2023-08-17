f56c844cfa5084474a67adf5b2c8d231203af529
Abdessattar <ghabi.1602@gmail.com>
2023-08-17 15:33:30 -0700

    3 and 5 multiples

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - print with Fizz and Buzz
+ * Description: Print but change the multiple of 5\
+ * with Buzz and 3 multiples with Fizz
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i < 101; i++)
+	{
+		if ((i % 3 == 0) && (i % 5 == 0))
+			printf("FizzBuzz ");
+		else if (i % 5 == 0)
+			printf("Buzz ");
+		else if (i % 3 == 0)
+			printf("Fizz ");
+		else
+			printf("%d ", i);
+	}
+	printf("\n");
+	return (0);
+}
