18639ea4362c541f93bdfebe644c9c2c3bd23c6f
Abdessattar <ghabi.1602@gmail.com>
2023-09-03 16:18:46 -0700

    Fix the code

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -10,17 +10,16 @@
 
 void puts_half(char *str)
 {
-	int i = 0;
+	int i, j, max;
 
+	i = 0;
 	while (str[i] != '\0')
 	{
 		i++;
 	}
-	if (i % 2 == 0)
-		i = i / 2;
-	else
-		i = (i - 1) / 2;
-	while (str[i] != '\0')
+	max = i;
+	j = max / 2;
+	while (j <= max)
 	{
 		_putchar(str[i]);
 		i++;
