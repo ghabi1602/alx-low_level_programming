9ee79807ae001083ee4f3a44315689930d5bf000
Abdessattar <ghabi.1602@gmail.com>
2023-09-03 09:33:57 -0700

    Print in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_rev - print reversed
+ * Description: print a string in reverse
+ * @s: string
+ *
+ * Return: 0
+ */
+
+void print_rev(char *s)
+{
+	int i = _strlen(s);
+
+	while (i >= 0)
+	{
+		_putchar(s[i]);
+		i--;
+	}
+	_putchar(10);
+}
