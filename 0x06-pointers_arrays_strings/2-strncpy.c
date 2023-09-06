ea3efb2c5bf8a793ed6a32de02703a8d748ebeae
Abdessattar <ghabi.1602@gmail.com>
2023-09-06 07:28:15 -0700

    Compare two strings

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -13,7 +13,17 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int j;
+	int i = 0;
 
+	dest[0] = '\0';
+	while (src[i] != '\0')
+	{
+		i++;
+	}
+	if (n > i)
+	{
+		n = i;
+	}
 	j = 0;
 	while ((src[j] != '\0') && (n > 0))
 	{
