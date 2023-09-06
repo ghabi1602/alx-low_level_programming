b64ec07c54b624bce233793ca8d8989df3692b4b
Abdessattar <ghabi.1602@gmail.com>
2023-09-06 15:58:03 -0700

    Function that encodes a string

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * *leet - encodes a string
+ * Description: function that encodes a string
+ * @s: string to encode
+ *
+ * Return: char
+ */
+char *leet(char *s)
+{
+        char aa[] = "aeotl";
+	char bb[] = "AEOTL";
+	char cc[] = "43701";
+	int i = 0;
+	int j;
+
+	while (s[i] != '\0')
+	{
+		j = 0;
+		while (aa[j] != '\0')
+		{
+			if (s[i] == aa[j] || s[i] == bb[j])
+			{
+				s[i] = cc[j];
+			}
+			j++;
+		}
+		i++;
+	}
+	return (s);
+}
