dca4b6f45be84854cded4c65391ffadec91e4574
Abdessattar <ghabi.1602@gmail.com>
2023-09-19 07:13:06 -0700

    Protect the header file from double inclusion

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,5 @@
-#ifndef VARIADIC_FUNCTIONS
-#define VARIADIC FUNCTIONS
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
 int sum_them_all(const unsigned int n, ...);
 int _putchar(char c);
 void print_numbers(const char *separator, const unsigned int n, ...);
