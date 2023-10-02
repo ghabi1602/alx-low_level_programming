56696b5aae1f3079c4293c13f07492fc2079ead1
Abdessattar <ghabi.1602@gmail.com>
2023-10-02 09:58:51 +0000

    Add numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - add numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: integer
+ */
+int main(int argc, char *argv[])
+{
+	int i, n = 0;
+
+	for (i = 1;i < argc; i++)
+	{
+		if (atoi(argv[i]))
+		{
+			n += atoi(argv[i]);
+		}
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+	}
+	printf ("%d\n", n);
+	return (0);
+}
