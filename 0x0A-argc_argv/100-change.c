35030df7770981a023da69bbd8787f996f17754e
Abdessattar <ghabi.1602@gmail.com>
2023-10-02 17:21:47 -0700

    Change the code

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,62 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-int check_change(int x, int coin);
 /**
- * main - prints minimum number of coins
- * @argc: number of arguments
- * @argv: array of arguments
- * Return: int
+ * main - prints the minimum number of coins
+ * @argc: number of command line arguments.
+ * @argv: array of arguments.
+ * Return: 0
  */
 int main(int argc, char *argv[])
 {
-	int cents, a, b, c, d, e;
-
+	int cents, n = 0;
 
 	if (argc == 1 || argc > 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
+
 	cents = atoi(argv[1]);
 
-	if (cents < 0)
+	while (cents > 0)
 	{
-		printf("0\n");
-		return (1);
+		if (cents >= 25)
+			cents -= 25;
+		else if (cents >= 10)
+			cents -= 10;
+		else if (cents >= 5)
+			cents -= 5;
+		else if (cents >= 2)
+			cents -= 2;
+		else if (cents >= 1)
+			cents -= 1;
+		n += 1;
 	}
-
-	a = check_change(cents, 25);
-	cents = cents - a * 25;
-	b = check_change(cents, 10);
-	cents -= b * 10;
-	c = check_change(cents, 5);
-	cents -= c * 5;
-	d = check_change(cents, 2);
-	cents -= d * 2;
-	e = check_change(cents, 1);
-	cents -= e;
-	printf("%d\n", a + b + c + d + e);
+	printf("%d\n", n);
 	return (0);
-}
-
-/**
- * check_change - prints how many coin within n
- * @x: int
- * @coin: int
- * Return: integer
- */
-
-int check_change(int x, int coin)
-{
-	if ((x - coin) < 0)
-	{
-		return (0);
-	}
-	else if ((x - coin) == 0)
-	{
-		return (1);
-	}
-	else
-		return (1 + check_change(x - coin, coin));
 }
