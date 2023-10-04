e96dd9fa14457cfb9b197c1dcfd6c581f39a611b
Abdessattar <ghabi.1602@gmail.com>
2023-10-04 15:10:22 +0000

    Modify the source code

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-char *copy(char *, char *, int);
+
 /**
  * *str_concat - concatinates two strings
  * @s1: string
@@ -11,7 +11,13 @@ char *copy(char *, char *, int);
 char *str_concat(char *s1, char *s2)
 {
 	int i, len1, len2, l1, l2;
-	char *s = "";
+	char *s;
+
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
 
 	len1 = 0;
 	while (*(s1 + len1) != '\0')
@@ -20,18 +26,6 @@ char *str_concat(char *s1, char *s2)
 	while (*(s2 + len2) != '\0')
 		len2++;
 
-	if (s1 == NULL)
-	{
-		s = copy(s, s2, len2);
-		return (s);
-	}
-	if (s2 == NULL)
-	{
-		s = copy(s, s1, len1);
-		return (s);
-	}
-	if (s1 == NULL && s2 == NULL)
-		return (NULL);
 
 	s = malloc(sizeof(char) * (len1 + len2) + 1);
 
@@ -56,20 +50,3 @@ char *str_concat(char *s1, char *s2)
 
 }
 
-/**
- * *copy - copies 1 string into s
- * @s: string to fill
- * @ch: string to copy
- * @x: ch 's length
- * Return: pointer to s
- */
-char *copy(char *s, char *ch, int x)
-{
-	int i;
-
-	for (i = 0; i < x; i++)
-	{
-		*(s + i) = *(ch + i);
-	}
-	return (s);
-}
