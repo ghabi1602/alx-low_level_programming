78ab79247e3b2d33ea14dc4ca1090ca40310e4a8
Abdessattar <ghabi.1602@gmail.com>
2023-10-10 19:34:53 +0000

    Delete the return statement

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -8,18 +8,19 @@
  */
 void print_dog(struct dog *d)
 {
-	if (!d)
-		return;
+	if (d)
+	{
+		if (d->name)
+			printf("Name: %s\n", d->name);
+		else
+			printf("Name: (nil)\n");
 
-	if (d->name == NULL)
-		printf("Name: (nil)\n");
-	else
-		printf("Name: %s\n", d->name);
+		printf("Age: %f\n", d->age);
 
-	printf("Age: %0.6f\n", d->age);
+		if (d->owner)
 
-	if (d->owner == NULL)
-		printf("Owner: (nil)");
-	else
-		printf("Owner: %s\n", d->owner);
+			printf("Owner: %s\n", d->owner);
+		else
+			printf("Owner: (nil)\n");
+	}
 }
