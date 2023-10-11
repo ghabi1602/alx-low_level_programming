94dd51086edbcab854522c27450ce394c20124f9
Abdessattar <ghabi.1602@gmail.com>
2023-10-11 17:44:06 +0000

    Executes a given function

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,25 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - executes a function
+ * @array: array of integers
+ * @size: size of the array
+ * @action: function pointer
+ * Return: void
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	long unsigned int i;
+
+	if (!action)
+		return;
+
+	if (!array)
+		return;
+
+	for (i = 0; i < size; i++)
+	{
+		(*action)(array[i]);
+		_putchar('\n');
+	}
+}
