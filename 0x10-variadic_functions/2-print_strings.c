65fa97729b302482176f6915434050e00215eef3
Abdessattar <ghabi.1602@gmail.com>
2023-10-12 15:38:58 +0000

    Fix the code

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -16,7 +16,12 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	va_start(args, n);
 
-	for (i = 0; i < n; i++)
+	if (n == 0)
+	{
+		printf("\n");
+		return;
+	}
+        for (i = 0; i < n; i++)
 	{
 		s = va_arg(args, char *);
 		if (!s)
