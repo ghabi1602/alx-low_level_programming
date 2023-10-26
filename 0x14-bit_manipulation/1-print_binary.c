a0be70cef9097c8f98972e4ce601aa94239650e6
Abdessattar <ghabi.1602@gmail.com>
2023-10-26 15:08:22 +0000

    Convert to binary

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * print_binary - converts a decimal into a binary
+ * @n: the integer to convert
+ * Return: void
+ */
+void print_binary(unsigned long int n)
+{
+	int i, j;
+	char s[30];
+
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+	if (n == 1)
+	{
+		_putchar('1');
+		return;
+	}
+	i = -1;
+	while (n != 0)
+	{
+		s[i++] = ((n - (n >> 1) * 2) + '0');
+		n = n >> 1;
+	}
+	j = i - 1;
+	while (j >= 0)
+	{
+		_putchar(s[j]);
+		j--;
+	}
+
+
+}
