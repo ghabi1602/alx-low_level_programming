c8ad7ee3b466bfc38e55770f8683863a026c4846
Abdessattar <ghabi.1602@gmail.com>
2023-10-26 15:36:03 +0000

    Fix the source code

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,31 +7,21 @@
  */
 void print_binary(unsigned long int n)
 {
-	int i, j;
-	char s[30];
 
 	if (n == 0)
 	{
 		_putchar('0');
 		return;
 	}
-	if (n == 1)
+	else if (n == 1)
 	{
 		_putchar('1');
 		return;
 	}
-	i = 0;
-	while (n != 0)
+	else
 	{
-		s[i] = ((n - (n >> 1) * 2) + '0');
-		n = n >> 1;
-		i++;
-	}
-	j = i - 1;
-	while (j >= 0)
-	{
-		_putchar(s[j]);
-		j--;
+		print_binary(n >> 1);
+		_putchar((n & 1) + '0');
 	}
 
 
