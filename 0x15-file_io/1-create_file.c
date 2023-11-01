89775f4e0232985b0f08d354943ee70faf36698f
Abdessattar <ghabi.1602@gmail.com>
2023-11-01 10:36:51 +0000

    Update

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,8 +14,7 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	
-	f = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 600);
+	f = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	if (!f)
 		return (-1);
